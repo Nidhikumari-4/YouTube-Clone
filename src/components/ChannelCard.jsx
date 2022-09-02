4d346105a75570d8a4e7a3c7c85af7b93cbe23d1
Nidhi Kumari <kumarinidhimain@gmail.com>
2022-09-02 20:42:45 +0530

    added code

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChannelCard = () => {
+  return (
+    <div>
+      <h1>ChannelCard</h1>
+    </div>
+  );
+};
+
+export default ChannelCard;
---
+++ src/components/Feed.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, Sidebar } from "./";
+
+const Feed = () => {
+  const [selectedCategory, setselectedCategory] = useState("New");
+
+  useEffect(() => {
+    fetchFromAPI(`Ssearch?part=snippet&q=${selectedCategory}`);
+  }, []);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright © 2022 JSM Media
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflow: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New
+          <span style={{ color: "#F31503" }}>Video</span>
+        </Typography>
+
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
